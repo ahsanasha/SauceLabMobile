0d74619837e9aeda931ce5b1e9b202d5c4aa1548
ahsanasha <ahsan.khuluk@anteraja.id>
2023-05-18 21:41:20 +0700

    Add packages add new scenario

---
+++ src/test/java/stepDefinitions/draw/DrawingSteps.java
@@ -0,0 +1,19 @@
+package stepDefinitions.draw;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DrawingSteps {
+    @And("User selects menu")
+    public void userSelectsMenu() {
+    }
+
+    @When("User select Drawing menu")
+    public void userSelectDrawingMenu() {
+    }
+
+    @Then("User should be navigate to Drawing page")
+    public void userShouldBeNavigateToDrawingPage() {
+    }
+}
